5ad0f487cd92b556cc40e81994136052396b012c
Gouda <mohamed8gouda@gmail.com>
2024-07-09 15:43:45 +0300

    Resized an image and converted it to grayscale. Added Gaussian noise to the grayscale image.

--- Frequency_Filters.py
+++ Frequency_Filters.py
@@ -10,13 +10,17 @@ A= io.imread('photo.jpeg');
 
 # Resize the image to the desired resolution (600, 800)
 A_resized = transform.resize(A, (600, 800), anti_aliasing=True)
+
 # Convert the resized image to grayscal
 Abw = color.rgb2gray(A_resized)
-# Add noise to the grayscale image
 
+# Add noise to the grayscale image
 B=Abw+0.5*np.random.randn(600,800)
+
+# Perform FFT and shift
 Bt=np.fft.fft2(B)
 Bts=fftshift(Bt)
+
 # Display the original and noisy images
 plt.subplot(1, 2, 1)
 plt.title("Resized Grayscale Image")
@@ -25,5 +29,4 @@ plt.imshow(Abw, cmap='gray')
 plt.subplot(1, 2, 2)
 plt.title("Noisy Image")
 plt.imshow(B, cmap='gray')
-
 plt.show()
